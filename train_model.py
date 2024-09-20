e49ec45d4f6998e116ab0641b92bcbf351a75a65
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-20 13:12:32 -0400

    add early stopping to prevent overfitting

--- train_model.py
+++ train_model.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 from tensorflow import keras
